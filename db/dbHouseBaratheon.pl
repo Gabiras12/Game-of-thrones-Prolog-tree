ab62c36bdfe7a3e9e8ffc329f09944007cbca686
Thiago Lima <thiagolima.unb@gmail.com>
2017-05-14 15:30:24 -0300

    adding patent relationship in House Baratheon

--- db/dbHouseBaratheon.pl
+++ db/dbHouseBaratheon.pl
@@ -18,5 +18,40 @@ female(selyse, florent).
 female(myrcella, baratheon).
 female(shireen, baratheon).
 female(mya, stone).
-female(bella).
 female(barra).
+female(bella).
+
+% parent(pai ou mae, sobrenome, filho, sobrenome).
+parent(ormund, baratheon, steffon, baratheon).
+parent(rhaelle, targaryen, steffon, baratheon).
+
+parent(steffon, baratheon, robert, baratheon).
+parent(cassana, estermont, robert, baratheon).
+
+parent(steffon, baratheon, renly, baratheon).
+parent(cassana, estermont, renly, baratheon).
+
+parent(steffon, baratheon, stannis, baratheon).
+parent(cassana, estermont, stannis, baratheon).
+
+parent(robert, baratheon, joffrey, baratheon).
+parent(cersei, lannister, joffrey, baratheon).
+
+parent(robert, baratheon, myrcella, baratheon).
+parent(cersei, lannister, myrcella, baratheon).
+
+parent(robert, baratheon, tommen, baratheon).
+parent(cersei, lannister, tommen, baratheon).
+
+parent(stannis, baratheon, shireen, baratheon).
+parent(selyse, florent, shireen, baratheon).
+
+parent(robert, baratheon, mya, stone).
+
+parent(robert, baratheon, gendry).
+
+parent(robert, baratheon, edric, storm).
+
+parent(robert, baratheon, barra).
+
+parent(robert, baratheon, bella).
