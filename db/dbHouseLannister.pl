f26c9f14d8862911f03bac524237a1f2601b9c32
Thiago Lima <thiagolima.unb@gmail.com>
2017-05-14 13:19:55 -0300

    adding parents os House Lannister

--- db/dbHouseLannister.pl
+++ db/dbHouseLannister.pl
@@ -1,3 +1,4 @@
+% male(nome, sobrenome).
 male(damon, lannister).
 male(tybolt, lannister).
 male(gerold, lannister).
@@ -20,6 +21,7 @@ male(robert, baratheon).
 male(joffrey, baratheon).
 male(tommen, baratheon).
 
+% female(nome, sobrenome).
 female(jeyne, marbrand).
 female(sarelle, lannister).
 female(joanna, lannister).
@@ -32,3 +34,71 @@ female(janei, lannister).
 female(ermesande, hayford).
 female(margaery, tyrell).
 female(myrcella, baratheon).
+female(joy, hill).
+
+% parent(pai ou mae, filho).
+parent(damon, tybolt).
+parent(damon, gerold).
+
+parent(tybolt, sarelle).
+
+parent(gerold, tywald).
+
+parent(gerold, tion).
+
+parent(gerald, tytos).
+
+parent(gerald, jason).
+
+parent(tytos, tywin).
+parent(jeyne, tywin).
+
+parent(tytos, kevan).
+parent(jeyne, kevan).
+
+parent(tytos, genna).
+parent(jeyne, genna).
+
+parent(tytos, tygget).
+parent(jeyne, gerion).
+
+parent(tytos, gerion).
+parent(jeyne, tygget).
+
+parent(tywin, cersei).
+parent(joanna, cersei).
+
+parent(tywin, jaime).
+parent(joanna, jaime).
+
+parent(tywin, tyrion).
+parent(joanna, tyrion).
+
+parent(kevan, lancel).
+parent(dorna, lancel).
+
+parent(kevan, willem).
+parent(dorna, willem).
+
+parent(kevan, martyn).
+parent(dorna, martyn).
+
+parent(kevan, janei).
+parent(dorna, janei).
+
+parent(tygget, tyrek).
+parent(darlessa, tyrek).
+
+parent(gerion, joy).
+
+parent(cersei, joffrey).
+parent(robert, joffrey).
+parent(jaime, joffrey).
+
+parent(cersei, myrcella).
+parent(robert, myrcella).
+parent(jaime, myrcella).
+
+parent(cersei, tommen).
+parent(robert, tommen).
+parent(jaime, tommen).
