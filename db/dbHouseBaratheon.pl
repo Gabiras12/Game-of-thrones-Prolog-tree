5a074521ab45590789d3ebf6575366fed1c7d630
Thiago Lima <thiagolima.unb@gmail.com>
2017-05-14 15:18:36 -0300

    adding male on House Baratheon

---
+++ db/dbHouseBaratheon.pl
@@ -0,0 +1,10 @@
+% male(nome, sobrenome).
+male(ormund, baratheon).
+male(steffon, baratheon).
+male(robert, baratheon).
+male(renly, baratheon).
+male(stannis, baratheon).
+male(joffrey, baratheon).
+male(tommen, baratheon).
+male(gendry).
+male(edric, storm).
