28da8975a784b682beb05006c08b81cd3c75553b
andersonms1 <andersoonn.ams@gmail.com>
2017-05-15 10:09:07 -0300

    adding males on House Stark

---
+++ db/dbHouseStark.pl
@@ -0,0 +1,64 @@
+% male(nome, sobrenome).
+
+male(benjen, stark).
+
+/*benjen's sons*/
+male(rickon, stark).
+male(bennard, stark).
+
+/*rickon sons*/
+male(cregan, stark).
+
+/*bennard sons*/
+male(benjen, stark).
+male(brandon, stark).
+male(elric, stark).
+
+/*creagan sons*/
+male(rickon, stark).
+male(jonnel, stark).
+male(edric, stark).
+male(barthogan, stark).
+male(brandon, stark).
+
+male(jon, umber).
+
+/*edric sons*/
+male(cregard, stark).
+male(torrhen, stark).
+
+/*brandon sons*/
+male(lonnel, snow).
+male(rodwell, stark).
+male(beron, stark).
+
+/*beron sons*/
+male(donnor, stark).
+male(willan, stark).
+male(artos, stark).
+male(errold, stark).
+male(rodrik, stark).
+
+/*willian sons*/
+male(brandon, stark).
+male(edwyle, stark).
+
+/*artons sons*/
+male(brandon, stark).
+male(benjen, stark).
+
+male(harold, rogers).
+
+/*edwyle sons*/
+male(rickard, sons).
+
+/*ryckard sons*/
+male(brandon, stark).
+male(eddard, stark).
+male(benjen, stark).
+
+/*eddard sons*/
+male(jon, snow).
+male(robb, stark).
+male(bran, stark).
+male(rickon, stark).
