d3aded2730122cd3125916022e2a1ce40c69d01f
Gustavo Moreira <gustavomn93@gmail.com>
2017-05-15 10:40:48 -0300

    Changing last name to string in Martells house

--- db/dbHouseMartell.pl
+++ db/dbHouseMartell.pl
@@ -1,71 +1,71 @@
-male(lewyn, martell).
-male(doran, martell).
-male(mors, martell).
-male(rhaegar, targaryen).
-male(olyvar, martell).
-male(oberyn, martell).
-male(quentyn, martell).
-male(trystane, martell).
-male(aegon, targaryen).
+male("lewyn martell").
+male("doran martell").
+male("mors martell").
+male("rhaegar targaryen").
+male("olyvar martell").
+male("oberyn martell").
+male("quentyn martell").
+male("trystane martell").
+male("aegon targaryen").
 
-female(princesa, dorne).
-female(elia, martell).
-female(ellaria, sand).
-female(mellario, norvos).
-female(arianne, martell).
-female(rhaenys, targaryen).
-female(obara, sand).
-female(nymeria, sand).
-female(tyene, sand).
-female(sarella, sand).
-female(elia, sand).
-female(obella, sand).
-female(dorea, sand).
-female(loreza, sand).
+female("princesa de dorne").
+female("elia martell").
+female("ellaria sand").
+female("mellario norvos").
+female("arianne martell").
+female("rhaenys targaryen").
+female("obara sand").
+female("nymeria sand").
+female("tyene sand").
+female("sarella sand").
+female("elia sand").
+female("obella sand").
+female("dorea sand").
+female("loreza sand").
 
-parent(lewyn, martell, doran, martell).
-parent(princesa, dorne, doran, martell).
+parent("lewyn martell", "doran martell").
+parent("princesa dorne", "doran martell").
 
-parent(lewyn, martell, mors, martell).
-parent(princesa, dorne, mors, martell).
+parent("lewyn martell", "mors martell").
+parent("princesa dorne", "mors martell").
 
-parent(lewyn, martell, elia, martell).
-parent(princesa, dorne, elia, martell).
+parent("lewyn martell", "elia martell").
+parent("princesa dorne", "elia martell").
 
-parent(lewyn, martell, olyvar, martell).
-parent(princesa, dorne, olyvar, martell).
+parent("lewyn martell", "olyvar martell").
+parent("princesa dorne", "olyvar martell").
 
-parent(lewyn, martell, oberyn, martell).
-parent(princesa, dorne, oberyn, martell).
+parent("lewyn martell", "oberyn martell").
+parent("princesa dorne", "oberyn martell").
 
-parent(doran, martell, arianne, martell).
-parent(mellario, norvos, arianne, martell).
+parent("doran martell", "arianne martell").
+parent("mellario norvos", "arianne martell").
 
-parent(doran, martell, quentyn, martell).
-parent(mellario, norvos, quentyn, martell).
+parent("doran martell", "quentyn martell").
+parent("mellario norvos", "quentyn martell").
 
-parent(doran, martell, trystane, martell).
-parent(mellario, norvos, trystane, martell).
+parent("doran martell", "trystane martell").
+parent("mellario norvos", "trystane martell").
 
-parent(rhaegar, targaryen, rhaenys, targaryen).
-parent(elia, martell, rhaenys, targaryen).
+parent("rhaegar targaryen", "rhaenys targaryen").
+parent("elia martell", "rhaenys targaryen").
 
-parent(rhaegar, targaryen, aegon, targaryen).
-parent(elia, martell, aegon, targaryen).
+parent("rhaegar targaryen", "aegon targaryen").
+parent("elia martell", "aegon targaryen").
 
-parent(oberyn, martell, obara, sand).
-parent(oberyn, martell, nymeria, sand).
-parent(oberyn, martell, tyene, sand).
-parent(oberyn, martell, sarella, sand).
+parent("oberyn martell", "obara sand").
+parent("oberyn martell", "nymeria sand").
+parent("oberyn martell", "tyene sand").
+parent("oberyn martell", "sarella sand").
 
-parent(oberyn, martell, elia, sand).
-parent(ellaria, sand, elia, sand).
+parent("oberyn martell", "elia sand").
+parent("ellaria sand", "elia sand").
 
-parent(oberyn, martell, obella, sand).
-parent(ellaria, sand, obella, sand).
+parent("oberyn martell", "obella sand").
+parent("ellaria sand", "obella sand").
 
-parent(oberyn, martell, dorea, sand).
-parent(ellaria, sand, dorea, sand).
+parent("oberyn martell", "dorea sand").
+parent("ellaria sand", "dorea sand").
 
-parent(oberyn, martell, loreza, sand).
-parent(ellaria, sand, loreza, sand).
+parent("oberyn martell", "loreza sand").
+parent("ellaria sand", "loreza sand").
