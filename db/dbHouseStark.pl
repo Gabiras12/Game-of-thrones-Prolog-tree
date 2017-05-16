cf1463e8849a7cb2f03e991ff29412e57fe8f40e
andersonms1 <andersoonn.ams@gmail.com>
2017-05-15 23:26:24 -0300

    fixing name string

--- db/dbHouseStark.pl
+++ db/dbHouseStark.pl
@@ -1,64 +1,64 @@
 % male(nome, sobrenome).
 
-male(benjen, stark).
+male("benjen stark").
 
 /*benjen's sons*/
-male(rickon, stark).
-male(bennard, stark).
+male("rickon stark").
+male("bennard stark").
 
 /*rickon sons*/
-male(cregan, stark).
+male("cregan stark").
 
 /*bennard sons*/
-male(benjen, stark).
-male(brandon, stark).
-male(elric, stark).
+male("benjen stark").
+male("brandon stark").
+male("elric stark").
 
 /*creagan sons*/
-male(rickon, stark).
-male(jonnel, stark).
-male(edric, stark).
-male(barthogan, stark).
-male(brandon, stark).
+male("rickon stark").
+male("jonnel stark").
+male("edric stark").
+male("barthogan stark").
+male("brandon stark").
 
-male(jon, umber).
+male("jon umber").
 
 /*edric sons*/
-male(cregard, stark).
-male(torrhen, stark).
+male("cregard stark").
+male("torrhen stark").
 
 /*brandon sons*/
-male(lonnel, snow).
-male(rodwell, stark).
-male(beron, stark).
+male("lonnel snow").
+male("rodwell stark").
+male("beron stark").
 
 /*beron sons*/
-male(donnor, stark).
-male(willan, stark).
-male(artos, stark).
-male(errold, stark).
-male(rodrik, stark).
+male("donnor stark").
+male("willan stark").
+male("artos stark").
+male("errold stark").
+male("rodrik stark").
 
 /*willian sons*/
-male(brandon, stark).
-male(edwyle, stark).
+male("brandon stark").
+male("edwyle stark").
 
 /*artons sons*/
-male(brandon, stark).
-male(benjen, stark).
+male("brandon stark").
+male("benjen stark").
 
-male(harold, rogers).
+male("harold rogers").
 
 /*edwyle sons*/
-male(rickard, sons).
+male("rickard stark").
 
 /*ryckard sons*/
-male(brandon, stark).
-male(eddard, stark).
-male(benjen, stark).
+male("brandon stark").
+male("eddard stark").
+male("benjen stark").
 
 /*eddard sons*/
-male(jon, snow).
-male(robb, stark).
-male(bran, stark).
-male(rickon, stark).
+male("jon snow").
+male("robb stark").
+male("bran stark").
+male("rickon stark").
