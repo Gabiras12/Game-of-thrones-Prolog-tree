0d6726ad43e65c2ed39e45caf331bf19eab81943
Marcos Dourado <marcosdourado92@gmail.com>
2017-05-17 00:36:16 -0300

    from 1st to 3rd tagaryen’s generations

---
+++ db/dbHouseTargaryen.pl
@@ -0,0 +1,11 @@
+male("aenar targaryen").
+male("gaemon targaryen").
+male("aegon targaryen").
+male("maegon targaryen").
+male("maegon targaryen").
+
+
+female("daenys targaryen").
+female("elaena targaryen").
+female("aerys targaryen").
+
