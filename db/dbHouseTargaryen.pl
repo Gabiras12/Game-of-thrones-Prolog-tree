8c50a94b061352bf8e4c8f556bf59fc214a4046d
Marcos Dourado <marcosdourado92@gmail.com>
2017-05-17 12:26:00 -0300

    all generations set

--- db/dbHouseTargaryen.pl
+++ db/dbHouseTargaryen.pl
@@ -15,6 +15,38 @@ male("viserys ii targaryen").
 male("aegon ii targaryen").
 male("aegon iii targaryen").
 male("aemond targaryen").
+male("daeron i targaryen").
+male("baelor i targaryen").
+male("aegon iv targaryen").
+male("daemon blackfyre").
+male("daeron ii targaryen").
+male("mariah martell").
+male("maron martell").
+male("brynden rivers").
+male("aegor rivers").
+male("aegon blackfyre").
+male("aemon blackfyre").
+male("daemon ii blackfyre").
+male("haemon blackfyre").
+male("baelor targaryen").
+male("aerys i targaryen").
+male("aelinor penrose").
+male("rhaegel targaryen").
+male("maekar i targaryen").
+male("valarr targaryen").
+male("matarys targaryen").
+male("daeron targaryen").
+male("aerion targaryen").
+male("meistre aemon").
+male("aegon v targaryen").
+male("jaehaerys ii targaryen").
+male("duncan targaryen").
+male("aerys ii targaryen").
+male("rhaegar targaryen").
+male("viserys iii targaryen").
+male("khal drogo").
+male("aegon targaryen").
+male("rhaego targaryen").
 
 
 female("daenys targaryen").
@@ -25,3 +57,18 @@ female("visenya targaryen").
 female("rhaenys targaryen").
 female("alysanne targaryen").
 female("rhaenyra targaryen").
+female("rhaena targaryen").
+female("elaena targaryen").
+female("daena targaryen").
+female("naerys targaryen").
+female("daenerys targaryen").
+female("shiera seastar").
+female("rhae seastar").
+female("daella seastar").
+female("rhaelle seastar").
+female("rhaella targaryen").
+female("jenny de pedravelhas").
+female("elia martell").
+female("daenerys i targaryen").
+female("rhaenys i targaryen").
+
