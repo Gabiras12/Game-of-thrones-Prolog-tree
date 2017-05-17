46b23e642507683ae4900908bd7a3ce7831a7400
Marcos Dourado <marcosdourado92@gmail.com>
2017-05-17 01:08:23 -0300

    10th generation targaryen

--- db/dbHouseTargaryen.pl
+++ db/dbHouseTargaryen.pl
@@ -1,11 +1,22 @@
 male("aenar targaryen").
 male("gaemon targaryen").
 male("aegon targaryen").
+male("aerys targaryen").
 male("maegon targaryen").
-male("maegon targaryen").
+male("baelon targaryen").
+male("aerion targaryen").
+male("maegor i targaryen").
+male("aegon i targaryen").
+male("orys baratheon").
+male("aenys i targaryen").
+male("orys baratheon").
 
 
 female("daenys targaryen").
 female("elaena targaryen").
-female("aerys targaryen").
+female("aelix targaryen").
+female("velena velaryon").
+female("visenya targaryen").
+female("rhaenys targaryen").
+female("rhaenys targaryen").
 
