22b2b223f7b969e7d6096f7f4c04b6b88e1b285e
Gustavo Moreira <gustavomn93@gmail.com>
2017-05-18 10:47:39 -0300

    Fixing house baratheon

--- db/dbHouseBaratheon.pl
+++ db/dbHouseBaratheon.pl
@@ -1,57 +1,57 @@
 % male(nome, sobrenome).
-male(ormund, baratheon).
-male(steffon, baratheon).
-male(robert, baratheon).
-male(renly, baratheon).
-male(stannis, baratheon).
-male(joffrey, baratheon).
-male(tommen, baratheon).
-male(gendry).
-male(edric, storm).
+male("ormund baratheon").
+male("steffon baratheon").
+male("robert baratheon").
+male("renly baratheon").
+male("stannis baratheon").
+male("joffrey baratheon").
+male("tommen baratheon").
+male("gendry").
+male("edric storm").
 
 % female(nome, sobrenome).
-female(rhaelle, targaryen).
-female(cassana, estermont).
-female(cersei, lannister).
-female(margaery, tyrell).
-female(selyse, florent).
-female(myrcella, baratheon).
-female(shireen, baratheon).
-female(mya, stone).
-female(barra).
-female(bella).
+female("rhaelle targaryen").
+female("cassana estermont").
+female("cersei lannister").
+female("margaery tyrell").
+female("selyse florent").
+female("myrcella baratheon").
+female("shireen baratheon").
+female("mya stone").
+female("barra").
+female("bella").
 
 % parent(pai ou mae, sobrenome, filho, sobrenome).
-parent(ormund, baratheon, steffon, baratheon).
-parent(rhaelle, targaryen, steffon, baratheon).
+parent("ormund baratheon", "steffon baratheon").
+parent("rhaelle targaryen", "steffon baratheon").
 
-parent(steffon, baratheon, robert, baratheon).
-parent(cassana, estermont, robert, baratheon).
+parent("steffon baratheon", "robert baratheon").
+parent("cassana estermont", "robert baratheon").
 
-parent(steffon, baratheon, renly, baratheon).
-parent(cassana, estermont, renly, baratheon).
+parent("steffon baratheon", "renly baratheon").
+parent("cassana estermont", "renly baratheon").
 
-parent(steffon, baratheon, stannis, baratheon).
-parent(cassana, estermont, stannis, baratheon).
+parent("steffon baratheon", "stannis baratheon").
+parent("cassana estermont", "stannis baratheon").
 
-parent(robert, baratheon, joffrey, baratheon).
-parent(cersei, lannister, joffrey, baratheon).
+parent("robert baratheon", "joffrey baratheon").
+parent("cersei lannister", "joffrey baratheon").
 
-parent(robert, baratheon, myrcella, baratheon).
-parent(cersei, lannister, myrcella, baratheon).
+parent("robert baratheon", "myrcella baratheon").
+parent("cersei lannister", "myrcella baratheon").
 
-parent(robert, baratheon, tommen, baratheon).
-parent(cersei, lannister, tommen, baratheon).
+parent("robert baratheon", "tommen baratheon").
+parent("cersei lannister", "tommen baratheon").
 
-parent(stannis, baratheon, shireen, baratheon).
-parent(selyse, florent, shireen, baratheon).
+parent("stannis baratheon", "shireen baratheon").
+parent("selyse florent", "shireen baratheon").
 
-parent(robert, baratheon, mya, stone).
+parent("robert baratheon", "mya stone").
 
-parent(robert, baratheon, gendry).
+parent("robert baratheon", "gendry").
 
-parent(robert, baratheon, edric, storm).
+parent("robert baratheon", "edric storm").
 
-parent(robert, baratheon, barra).
+parent("robert baratheon", "barra").
 
-parent(robert, baratheon, bella).
+parent("robert baratheon", "bella").
