af207a13156840500b1b7a15e5935aaf3e0a0482
Gabriel Araújo <gabrieldea.silva@gmail.com>
2017-05-19 04:35:09 -0300

    Add more features

--- familyRelationships.pl
+++ familyRelationships.pl
@@ -2,13 +2,23 @@ mother(X,Y) :-
   parent(X,Y),
   female(X).
 
+mother(X):-
+  mother(A,X),
+  write(A), write(' is the mother of '), write(X).
+
 father(X,Y) :-
   parent(X,Y),
   male(X).
 
+father(X) :- father(A,X), write(A), write(' is the father of '), write(X).
+
 child(X,Y) :-
   parent(A,X),
   Y == A.
+
+childs(X) :-
+  child(A,X),
+  write(A), write(' is the child of '), write(X).
 
 sibling(X,Y) :-
   father(A, X),
@@ -19,12 +29,13 @@ sibling(X,Y) :-
 
 brother(X,Y) :-
   sibling(X,Y),
-  male(X).
+  male(X),
+  write(X), write(' is the brother of '), write(Y).
 
 sister(X,Y) :-
   sibling(X,Y),
   female(X),
-  format("~s", [X]).
+  write(X), write(' is the sister of '), write(Y).
 
 uncle(X,Y) :-
   father(A,Y), brother(X,A);
@@ -58,3 +69,54 @@ generations(X, Y, Num):-
   parent(X,Z),
   generations(Z, Y, Num2),
   Num is Num2 + 2.
+
+countKill(X,Num):-
+  findall(X, killed(X,_), Xs),
+  length(Xs, Num).
+
+hasKilled(X):-
+  countKill(X,Num),
+  write(X), write(' has killed '), write(Num).
+
+sequenceOfKills(X):-
+  killed(A,X),
+  write(A), write(' has killed '), write(X),nl,
+  sequenceOfKills(A).
+
+countMen(X) :-
+  findall(A, male(A), As),
+  length(As, X).
+
+goodManToMarige:-
+  countMen(NumMen),
+  write('You\'ve '), write(NumMen), write(' options. But...'), nl,
+  findall(Man, (male(Man), notAssassins(Man)), L),
+  length(L, N),
+  write('Only '), write(N), write(' Are not killers. And...'), nl,
+  findall(Man, (male(Man), notAssassins(Man), hasLands(Man)), L1),
+  length(L1, N1),
+  write('Only '), write(N1), write(' of then has land...But...'), nl,
+  findall(Man, (male(Man), notAssassins(Man), hasLands(Man), isBeautiful(Man)), L2),
+  length(L2, N2),
+  write('Only '), write(N2), write(' of then are beautiful...But...'), nl,
+  whoHasMoreLands(L2, Best),
+  write(Best), write(' has more lands.. so.. you should marry him.').
+
+notAssassins(X):-
+  killed(X,_) -> false; true.
+
+hasLands(X):-
+  ownsLand(X) -> true; false.
+
+whoHasMoreLands([H],H).
+whoHasMoreLands([H|T], Result):-
+  whoHasMoreLands(T, P),
+  numberOfLands(H,X),
+  numberOfLands(P,K),
+  (X >= K -> updateResult(Result, H); updateResult(Result, P)).
+
+updateResult(H,H).
+
+numberOfLands(X,R):-
+  findall(A, ownsLand(X), As),
+  length(As, R).
