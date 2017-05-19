b5faa9579c3dfd48844e4518573afa079972898a
Marcos Dourado <marcosdourado92@gmail.com>
2017-05-18 22:32:34 -0300

    season 5 deaths

---
+++ db/dbKillings.pl
@@ -0,0 +1,35 @@
+killed("aryah stark", "walder frey").
+killed("aryah stark", "lothar frey").
+killed("aryah stark", "walder rivers").
+killed("aryah stark", "waif").
+killed("aryah stark", "meryn trant").
+
+% burned alive in a wildfire explosion orchestrated by Cersei
+killed("cersei lannister", "margaery tyrell").
+killed("cersei lannister", "kevan lannister").
+killed("cersei lannister", "mace tyrell").
+killed("cersei lannister", "loras tyrell").
+killed("cersei lannister", "the high sparrow").
+
+killed("sansa stark", "ramsay bolton").
+
+killed("ramsay bolton", "rickon stark").
+
+killed("jaime lannister", "aerys ii targaryen").
+
+killed("daenerys i targaryen", "khal moro").
+
+killed("jolly", "jon snow").
+
+killed("jon snow", "olly").
+killed("jon snow", "alliser thorne").
+
+killed("ramsay bolton", "osha").
+killed("ramsay bolton", "walda bolton").
+killed("ramsay bolton", "roose bolton").
+
+killed("ellaria sand", "doran martell").
+
+
+
+
